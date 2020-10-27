1cbd8b1f39c8ccb7513797c0fe809b6b440d2161
osipenkoDenis <denis.osipenko@inbox.ru>
2020-10-27 21:48:39 +0300

    feat:complete dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if(!Array.isArray(members)){
+    return false;
+  }
+  let arr = [];
+  for(let i = 0;i < members.length; i++){
+    if(typeof members[i] === 'string'){
+      arr.push(members[i].trim().toUpperCase());
+    }
+  }
+  let array = arr.sort();
+  let name = '';
+  for(let elem of array){
+    name+=elem[0];
+  }
+  return name
 };
